388a9c8424a4985589b5b6b987abe9a6a69a45b7
gleysonnnnn <44455897+gleysonnnnn@users.noreply.github.com>
2020-06-29 18:00:43 -0300

    Create gpgcracker.sh

---
+++ gpgcracker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#GPGCracker (em desenvolvimento)
+
+if [ "$#" != 2 ]; then
+        echo "USAGE: $0 file.gpg wordlist.txt"
+else
+
+
+while read pass; do
+        gpg --batch --yes --passphrase "$pass" "$1" &> /dev/null
+        if [ "$?" == 0 ]; then
+                echo
+                echo "SUCESS - $pass"
+                echo
+                break
+        else
+                echo "FAILED - $pass"
+        fi
+done < "$2"
+fi
